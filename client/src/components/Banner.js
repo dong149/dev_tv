b8897e10d174c1f33dd8344ea626b0f530d298e2
DANNIEL <donghoon149@gmail.com>
2020-06-23 19:29:40 +0900

    fix bugs

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -11,16 +11,24 @@ require("dotenv").config();
 const Banner = (props) => {
   const { close } = props;
   const [imageLoaded, setImageLoaded] = useState(false);
-
+  const [image2Loaded, setImage2Loaded] = useState(false);
   return (
     <>
-      <div className="banner">
+      <div
+        className={`banner banner-${
+          imageLoaded && image2Loaded ? "visible" : "hidden"
+        }`}
+        onLoad={() => setImage2Loaded(true)}
+      >
         <span className="item"></span>
         <div className="banner-center">
           <img
-            className="banner-logo"
-            src="/banner-text.png"
+            className={`banner-logo-smooth banner-logo-${
+              imageLoaded && image2Loaded ? "visible" : "hidden"
+            }`}
+            src="/banner.png"
             alt="devtv 배너"
+            onLoad={() => setImageLoaded(true)}
           />
         </div>
       </div>
